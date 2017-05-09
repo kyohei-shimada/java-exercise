e7ed0c765b86c650d29cd86dc5e42bc8aec2af18
Kyohei Shimada <fl95to03pj30sq12+github@gmail.com>
2017-05-10 07:40:50 +0900

    answered Q.1-2, 1-3

---
+++ chapter1/1_2.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String args[]) {
+        int x = 11;
+        System.out.println("x=" + x);
+    }
+}
---
+++ chapter1/1_3.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String args[]) {
+        int x = 13;
+        int y = 17;
+        System.out.println("x=" + x + ",y=" + y);
+    }
+}
